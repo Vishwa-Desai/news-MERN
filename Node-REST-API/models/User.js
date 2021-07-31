395648926c8aa5087690a4b56a422deec72aa705
Vishwa-Desai <vishwadesai2408@gmail.com>
2021-07-31 10:54:02 +0530

    First Commit

---
+++ Node-REST-API/models/User.js
@@ -0,0 +1,58 @@
+const mongoose=require("mongoose");
+
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        min:3,
+        max:20,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        min:10
+    },
+    password:{
+        type:String,
+        required:true,
+        min:8,
+        max:16,
+    },
+    profilePicture:{
+        type:String,
+        default:"",
+    },
+    coverPicture:{
+        type:String,
+        default:"",
+    },
+    followers:{
+        type:Array,
+        default:[],
+    },
+    followings:{
+        type:Array,
+        default:[],
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
+    desc:{
+        type:String,
+        max:50,
+    },
+    city:{
+        type:String,
+        max:50
+    },
+    from:{
+        type:String,
+        max:50
+    }, 
+},
+{timestamps:true}
+);
+
+module.exports=mongoose.model("User",UserSchema);
---
+++ Node-REST-API/routes/posts.js
@@ -0,0 +1,97 @@
+const router=require("express").Router();
+
+const Post=require("../models/Post");
+const User = require("../models/User");
+
+//create post
+    router.post("/",async (req,res)=>{
+        const newPost=new Post(req.body);
+        try{
+            const savedPost=await newPost.save();
+            res.status(200).json(savedPost);
+        }catch(err){
+            res.status(500).json(err);   
+        }
+    });
+
+//update post
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("the post has been updated");
+      } else {
+        res.status(403).json("you can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//delete post
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json("the post has been deleted");
+      } else {
+        res.status(403).json("you can delete only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  //like post
+
+  router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ $push: { likes: req.body.userId } });
+        res.status(200).json("The post has been liked");
+      } else {
+        await post.updateOne({ $pull: { likes: req.body.userId } });
+        res.status(200).json("The post has been disliked");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+//get a post
+
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//get timeline posts (following)
+
+router.get("/timeline/all", async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await Post.find({ userId: currentUser._id });
+      //why i have used promise here?
+      //here we have to fetch all the posts so we use map
+      //and with map we have to use Promise.all otherwise it will not fetch all the posts
+      //*********** Note ******* Learn more about this ***** */
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId });
+        })
+      );
+      res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports=router;
